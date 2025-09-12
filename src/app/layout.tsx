eb3602cb8895b00c846163c4c321c7d2a115f777
Your Name <your-email@example.com>
2025-09-12 00:00:07 -0700

    tawk to script and widget

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TawkToScript from "@/components/TawkToScript";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         suppressHydrationWarning={true}
       >
         {children}
+        <TawkToScript />
       </body>
     </html>
   );
---
+++ src/components/TawkToScript.tsx
@@ -0,0 +1,23 @@
+import Script from 'next/script';
+
+export default function TawkToScript() {
+  return (
+    <Script
+      id="tawk-to"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/68c3c08b0b3548192e857ea9/1j4uag8u1';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();
+        `,
+      }}
+    />
+  );
+}
